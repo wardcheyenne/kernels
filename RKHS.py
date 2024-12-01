9c6db5e11b1d86a2827e7975b27211b7ab8ceb66
ward <ward@ward.lan>
2024-12-01 07:53:57 -0800

    RKHS implementation

---
+++ RKHS.py
@@ -0,0 +1,19 @@
+import numpy as np
+import random
+
+feature_map = [lambda x: x**2, lambda x: x, lambda x: 1]
+data_space = [np.random.randint(1, 20) for _ in range(5)]
+weights = [1, -1, 1]
+
+function_space = [ #Also known as the RKHS
+    lambda x, f=f, c=c: c * f(x) 
+    for f, c in zip(feature_map, weights)
+]
+
+def eval_RKHS(points, function_space):
+    eval_points = []
+    for i in points:
+        eval_points.append([f(i) for f in function_space])
+    return np.dot(eval_points[0], eval_points[1]), eval_points, points
+
+print(eval_RKHS(random.sample(data_space, 2), function_space))
